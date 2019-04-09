1dea48e894f50dda7c8089f1b6523a5f7f321baa
Selina Erhabor <selinaerhabor@gmail.com>
2019-04-09 23:45:09 +0000

    Added the links to templates to allow user to log in and log out from their account when using the website

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
+
+#Home Page
+def index(request):
+    return render(request, 'index.html')
+    
+#For Log out Tab
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
+    
+#For Log in Tab
+def login(request):
+    return render(request,'login.html')
