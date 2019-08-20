b1e5fbdf39dd20a6b390082113fcd2be0039eed6
Selina Erhabor <selinaerhabor@gmail.com>
2019-08-20 14:39:02 +0000

    Amended comments in admin.py file

--- learningforadults/admin.py
+++ learningforadults/admin.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Adultsquestion
 
-# Learning For Kids Quiz Model added to Django Admin
+# Learning For Adults Quiz Model added to Django Admin
 admin.site.register(Adultsquestion)
