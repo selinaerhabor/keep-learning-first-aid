fa9d5574c3c4d5b5155650806195c001b3035b22
Selina Erhabor <selinaerhabor@gmail.com>
2019-08-29 09:14:18 +0000

    Configured and added template for cart to display when user clicks on the cart link at the top of the page to be prompted to continue to checkout page after reviewing items in their cart.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+# Cart contents render on page
+def view_cart(request):
+    return render(request, "cart.html")
+
+# Adds a quantity of the specified product to the cart
+def add_to_cart(request, id):
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('sale'))
+
+
+# Adjusts the quantity of the specified product to the specified amount
+def adjust_cart(request, id):
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
