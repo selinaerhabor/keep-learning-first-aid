e55254170e0bc1a73e53c1d9e009978c141effb2
Selina Erhabor <selinaerhabor@gmail.com>
2019-08-29 22:43:16 +0000

    Created view_product pg to allow users to view a products' full details. Page url and view has been updated in files.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import sale
+from .views import sale, view_product
 
 urlpatterns = [
-    url(r'^sale/', sale, name='products'),
+    url(r'^sale/$', sale, name='products'),
+    url(r'^view_product/(?P<id>\d+)/$', view_product, name="view_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
-# Create your views here.
+"""
+View to show First Aid Learning Materials and Equipment available for 
+purchase on the website
+"""
 def sale(request):
-    products = Product.objects.all()
-    return render(request, "sale.html", {"products": products})
+    
+    products = Product.objects.all().order_by('category')
+    page = request.GET.get('page', 1)
+    
+    paginator = Paginator(products, 10)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    
+        
+    return render(request, "products.html", {"products": products})
+    
+
+def view_product(request, id):
+    """
+    This view displays the full details of a product to the user
+    """
+    products = get_object_or_404(Product, pk=id)
+    return render(request, "viewproduct.html", {"products": products})
+
+  
