177cd023df2850598b0bcee42d3d88f006310b91
Selina Erhabor <selinaerhabor@gmail.com>
2019-08-30 10:38:34 +0000

    Created Order app , updated views, urls and relevant files accordingly. Order template also created and stored within the app.

---
+++ order/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from products.models import Product
+
+# Order Checkout Model
+class Order(models.Model):
+    
+    title_choices = (
+        ("REVEREND", "Reverend"),
+        ("SIR", "Sir"),
+        ("DAME", "Dame"),
+        ("MR", "Mr"),
+        ("MRS", "Mrs"),
+        ("MS", "Ms"),
+        ("MISS", "Miss"),
+    )
+    title = models.CharField(max_length=50, choices=title_choices, blank=False)
+    
+    first_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    email = models.CharField(max_length=60, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    house_number = models.CharField(max_length=40, blank=False)
+    address_line1 = models.CharField(max_length=40, blank=False)
+    address_line2 = models.CharField(max_length=40, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=True)
+    postcode = models.CharField(max_length=20, blank=False)
+    phone_number = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}{3}".format(self.id, self.date, self.first_name, self.last_name)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    product = models.ForeignKey(Product, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.product.name, self.product.price)
