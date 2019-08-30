ed1c7093502b35d2f10d1679ddad7f84627d8b23
Selina Erhabor <selinaerhabor@gmail.com>
2019-08-30 12:30:32 +0000

    Amended positioning of order and payment forms using django crispy form and imported package into forms and views. Form information now displaying more neatly. Added an amend cart button to allow users to amend items in their cart without having to press back button on browser.

--- order/views.py
+++ order/views.py
@@ -5,6 +5,7 @@ from .forms import MakePaymentForm, OrderForm
 from .models import OrderLineItem
 from django.conf import settings
 from django.utils import timezone
+from crispy_forms.helper import FormHelper
 from products.models import Product
 import stripe
 
