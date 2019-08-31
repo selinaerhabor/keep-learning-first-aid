1220e79815b6e80b4adb532274ee9f3f8accdf77
Selina Erhabor <selinaerhabor@gmail.com>
2019-08-31 18:47:54 +0000

    removed modal toggle JS from custom.js file as modals are now working

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('#myCarousel').on('slide.bs.carousel');
     $('.collapse').collapse();
-    $('#myModal').modal('toggle');
     
     $(function () {
       $('[data-toggle="tooltip"]').tooltip();
