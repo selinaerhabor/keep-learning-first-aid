a26b3449cedb531f37fe1d8a8c97e6c2066cf402
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-05 18:04:32 +0000

    Added the view empty cart to cart views.py file and established the url to allow users to empty their cart when needed to restart their shopping.

--- cart/views.py
+++ cart/views.py
@@ -1,21 +1,34 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from products.models import Product
 
-# Cart contents render on page
+
+""" 
+Items in cart render on cart.html page
+"""
 def view_cart(request):
     return render(request, "cart.html")
 
-# Adds a quantity of the specified product to the cart
+
+"""
+Allows user to add a quantity of the specified product to the cart, by default
+user can add quantity of 1
+"""
 def add_to_cart(request, id):
     quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
     return redirect(reverse('sale'))
 
 
-# Adjusts the quantity of the specified product to the specified amount
+"""
+Adjusts the quantity of the specified product to the specified amount
+"""
 def adjust_cart(request, id):
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
@@ -27,3 +40,12 @@ def adjust_cart(request, id):
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+"""
+Empties all items in shopping cart
+"""
+def empty_cart(request):
+    cart = request.session.delete()
+    return redirect(reverse('view_cart'), {"cart": cart})
+    
