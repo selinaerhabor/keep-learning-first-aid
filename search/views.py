a1c057c71a89777610d8b2c98e4b1af1d2230854
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-07 19:01:48 +0000

    Added pagination to search results to make results more presentable and consistent

--- search/views.py
+++ search/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from products.models import Product
 
 # Create your views here.
 def do_search(request):
     products = Product.objects.filter(category__icontains=request.GET['q'])
-    return render(request, "products.html", {"products": products})
+    page = request.GET.get('page', 1)
+    page_number = page
+    paginator = Paginator(products, 9)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, "products.html", {"products": products, "page_number": page_number})
