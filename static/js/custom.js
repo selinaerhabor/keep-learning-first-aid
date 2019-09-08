5b60b4c0a70e4d58fb0fb8d63937ad6b166bcd21
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-08 00:48:13 +0000

    Comments added to JQuery code.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
-    $('#myCarousel').on('slide.bs.carousel');
-    $('.collapse').collapse();
-    
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip();
-    });
+  // Carousel Slide Control
+  $('#myCarousel').on('slide.bs.carousel');
+  
+  // Collapse class
+  $('.collapse').collapse();
+  
+  // Tooltip (Info that appears when user hovers over certain features)
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+  
+  // Controls CSS for Active class for current page displayed on xs & sm screens only
+  $("li").click(function(){
+    $("li").addClass("active");
+  });
 });
