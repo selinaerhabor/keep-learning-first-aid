2c0ef80fd9302df7ad02ff71603e7f56397f0e20
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-10 23:23:20 +0000

    Created and documented tests for accounts app.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from accounts.forms import UserAccountForm, UserRegistrationForm
 
-# Create your tests here.
+# Testing Registration Form works as intended
+class RegistrationFormTest(TestCase):
+    
+    def registration_form_denied_for_password_mismatch(self):
+        register_form = UserRegistrationForm({
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'email': 'john.smith@test.com',
+            'password1': 'test123',
+            'password2': 'test987',
+        })
+        self.assertFalse(register_form.is_valid())
+        
+        
+    def registration_form_requires_five_values(self):
+        register_form = UserRegistrationForm({
+            'first_name': 'John',
+            'last_name': 'Smith',
+            'email': 'john.smith@test.com',
+            'password1': 'test123',
+            'password2': 'test123',
+        })
+        self.assertTrue(register_form.is_valid())
+        
+        
+# Testing Login Form works as intended
+class LoginFormTest(TestCase):
+    
+    def login_form_requires_password(self):
+        login_form = UserAccountForm({
+            'email': 'john.smith@test.com',
+        })
+        self.assertFalse(login_form.is_valid())
+        
+        
+    def login_form_test(self):
+        login_form = UserAccountForm({
+            'username': 'john.smith@test.com',
+            'password': 'test123',
+        })
+        self.assertTrue(login_form.is_valid())
+
+
+    def login_only_for_registered_users(self):
+        login_user = self.client.login(username='john.s@test.com', password='test123')
+        self.assertFalse(login_user)
