8ffc4302b33594315174c227dfba8e13687dbf74
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-12 05:27:16 +0000

    Changed Debug to False and adjustment made to requirements.txt file

--- keeplearningfirstaid/settings.py
+++ keeplearningfirstaid/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
                 '0640aa05cfb6469bb38873f59dc178dc.vfs.cloud9.us-east-1.amazonaws.com',
@@ -160,7 +160,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % 'keeplearningfirstaid'
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-STATIC_URL = os.environ.get('STATIC_URL')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
