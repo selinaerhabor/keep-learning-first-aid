1bd05b70907cb1f6e8574dd00084925351939411
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-12 00:50:22 +0000

    Added missing category E-books to the dropdown filter in products list and configured its URL to allow access to filter and find E-Books.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
-from .views import sale, view_product, get_books, get_posters, get_extras, get_cds, get_courses, get_firstaidkits, get_manikins
+from .views import sale, view_product, get_books, get_ebooks, get_posters, get_extras, get_cds, get_courses, get_firstaidkits, get_manikins
 
 urlpatterns = [
     url(r'^sale/$', sale, name='sale'),
     url(r'^view_product/(?P<id>\d+)/$', view_product, name="view_product"),
     url(r'^sale/books/$', get_books, name="get_books"),
+    url(r'^sale/ebooks/$', get_ebooks, name="get_ebooks"),
     url(r'^sale/posters/$', get_posters, name="get_posters"),
     url(r'^sale/extras/$', get_extras, name="get_extras"),
     url(r'^sale/cds/$', get_cds, name="get_cds"),
