c05974706677c32d070c997a365c91fc52cf9e1c
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-12 01:49:43 +0000

    Created automated tests for order forms ensuring test names begin with test_

--- order/tests.py
+++ order/tests.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
 from .forms import MakePaymentForm, OrderForm
-from products.models import Product
 
 # Testing Make Payment Form works properly
 class MakePaymentFormTest(TestCase):
-    def submit_with_required_input_test(self):
+    def test_submit_with_required_input(self):
         payment_form = MakePaymentForm({
                     'credit_card_number': '4242424242424242', 
                     'cvv': "567",
@@ -15,7 +14,7 @@ class MakePaymentFormTest(TestCase):
         self.assertTrue(payment_form.is_valid())
         
     
-    def submit_without_required_input_test(self):
+    def test_submit_without_required_input(self):
         payment_form = MakePaymentForm({
                     'credit_card_number': '', 
                     'cvv': '567', 
@@ -27,7 +26,7 @@ class MakePaymentFormTest(TestCase):
 
 # Testing Order Form works properly
 class OrderFormTest(TestCase):
-    def submit_required_input_test(self):
+    def test_submit_required_input(self):
         order_form = OrderForm({
                     'title': 'Mr',
                     'first_name': 'John',
@@ -44,7 +43,7 @@ class OrderFormTest(TestCase):
         
     
     
-    def submit_missing_required_input_test(self):
+    def test_submit_missing_required_input(self):
         order_form = OrderForm({
                     'title': 'Mr',
                     'first_name': 'John',
