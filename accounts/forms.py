3f4ce552e66897f042f116e1605a1a645faab0b7
Selina Erhabor <selinaerhabor@gmail.com>
2019-09-12 07:56:06 +0000

    Added django crispy form helper

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit
 
 
 # Allows user to create an account
